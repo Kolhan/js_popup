815a1bbe6e4a35e0c93ff645146243e56de6433b
Kolhan <kolhan@mail.ru>
2022-01-27 10:09:44 +0500

    add: blur и esc btn

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const images = ['img/bird.svg', 'img/cat.svg', 'img/dog.svg']
+const images = [
+    'img/bird.svg', 
+    'img/cat.svg', 
+    'img/dog.svg',
+    'img/hamster.svg'
+]
 
+//инициализация кнопок
 images.forEach(el => {
     const img = document.createElement("div");
     img.className = 'mini__item'
@@ -11,21 +17,42 @@ images.forEach(el => {
 })
 
 
+//Всплывающее окно
 const popup = document.querySelector('.popup')
 
+const popupLightBox = document.querySelector('.popup__ligthbox')
 const popupContent = popup.firstElementChild;
 const popupClose = popup.lastElementChild;
 
-function closePopup(event) {
-    event.target.parentElement.classList.remove('popup_active')
+function closePopup(event) {    
+    popup.classList.remove('popup_active')
+    popupLightBox.classList.remove('popup_active')
+    
 }
+
+function keydown(event) {
+    event.preventDefault()
+    if (event.key == 'Escape' ||  (event.key == 'F4' && event.altKey) ) {
+        closePopup(event)
+    }     
+    console.log(event)
+}
+
 
 function showPopup(text) {
     popup.classList.add('popup_active')
     popupContent.innerHTML = text;
+
+    popupLightBox.classList.add('popup_active')
 }
 
 popupClose.addEventListener('click', closePopup)
+popupLightBox.addEventListener('click', closePopup)
+document.body.addEventListener('keydown', keydown)
+
+//Думаю реализация Alt+F4 не возможна
+window.webContents.on("before-input-event",(event,input)=>{
+    if(input.code=='F4'&&input.altKey)event.preventDefault();});
 
 /*
     #hw1 link(git)
